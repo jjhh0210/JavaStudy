8fcdd381dd3f8a93fe7f44f8fa1945ba5a858891
jjhh0210 <jjhh0210@github.com>
2023-01-21 01:22:25 +0900

    edit dijkstra

--- src/알고리즘강의/src/9_Greedy/Dijkstra.java
+++ src/알고리즘강의/src/9_Greedy/Dijkstra.java
@@ -8,24 +8,24 @@ class Edge implements Comparable<Edge>{
     }
     @Override
     public int compareTo(Edge ob){
-        return this.cost-ob.cost;
+        return this.cost-ob.cost;   //오름차순(작은 것을 poll)
     }
 }
 
 class Dijkstra {
-    static int n, m;
-    static ArrayList<ArrayList<Edge>> graph;
-    static int[] dis;
+    static int n, m;    //n = 정점의 개수 , m = 간선의 개수
+    static ArrayList<ArrayList<Edge>> graph;    
+    static int[] dis;   //시작정점부터 모든 정점까지의 최단 거리(비용)
     public static void solution(int v){
         PriorityQueue<Edge> pQ = new PriorityQueue<>();
-        pQ.offer(new Edge(v, 0));
+        pQ.offer(new Edge(v, 0));   //시작 지점 우선 offer
         dis[v]=0;
         while(!pQ.isEmpty()){
-            Edge tmp=pQ.poll();
-            int now=tmp.vex;
-            int nowCost=tmp.cost;
-            if(nowCost>dis[now]) continue;
-            for(Edge ob : graph.get(now)){
+            Edge tmp=pQ.poll(); //poll 할 때 가장 작은 것을 poll(작은것이 우선이 되도록 설정했기 때문)
+            int now=tmp.vex;    //현재 정점
+            int nowCost=tmp.cost;   //현재 정점까지의 비용
+            if(nowCost>dis[now]) continue;  //노트 참고, dis에의 현재 정점까지의 최소비용보다 더 큰 비용으로 뻗어나갈 이유가 없으므로 거름
+            for(Edge ob : graph.get(now)){  //현재 정점과 인접한 정점들을 탐색한다.
                 if(dis[ob.vex]>nowCost+ob.cost){
                     dis[ob.vex]=nowCost+ob.cost;
                     pQ.offer(new Edge(ob.vex, nowCost+ob.cost));
@@ -51,10 +51,10 @@ class Dijkstra {
             int c=kb.nextInt();
             graph.get(a).add(new Edge(b, c));
         }
-        solution(1);
+        solution(1);    //1번정점에서 출발
         for(int i=2; i<=n; i++){
             if(dis[i]!=Integer.MAX_VALUE) System.out.println(i+" : "+dis[i]);
-            else System.out.println(i+" : impossible");
+            else System.out.println(i+" : impossible"); //처음에 초기화 했던 값과 같으면 impossible이라고 출력
         }
     }
 }
